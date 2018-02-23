2bbe240faafab9a156cac94f227cb90780a30006
SH\itdgyt01 <879390870@qq.com>
2018-02-23 17:56:02 +0800

    springboot-mybatis 注解

---
+++ springboot_annotation_mybatis/src/main/java/com/tom/User.java
@@ -0,0 +1,14 @@
+package com.tom;
+
+import lombok.Data;
+
+/**
+ * @Author tom
+ * @Date 2018/2/22 17:00
+ */
+@Data
+public class User {
+  private Long id;
+  private String name;
+  private Integer age;
+}
---
+++ springboot_annotation_mybatis/src/test/java/com/tom/SpringbootAnnotationMybatisApplicationTests.java
@@ -0,0 +1,28 @@
+package com.tom;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class SpringbootAnnotationMybatisApplicationTests {
+	@Autowired
+	private UserMapper userMapper;
+
+	@Test
+	@Rollback(true)
+	public void findByName() throws Exception {
+		userMapper.insert("AAA", 20);
+		User u = userMapper.findByName("AAA");
+		Assert.assertEquals(20, u.getAge().intValue());
+	}
+
+}
